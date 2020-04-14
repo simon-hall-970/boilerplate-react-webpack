7ee91a69a66f0240572f89f3a4bb7bb86d433fab
Ross Jourdain <>
2020-04-15 10:18:20 +1200

    Tweak layout and deps

--- server/server.js
+++ server/server.js
@@ -1,9 +1,8 @@
-const path = require('path')
 const express = require('express')
 
 const server = express()
 
 server.use(express.json())
-server.use(express.static(path.join(__dirname, './public')))
+server.use(express.static('public'))
 
 module.exports = server
