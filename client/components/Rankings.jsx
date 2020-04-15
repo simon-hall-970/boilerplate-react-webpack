167fa488eeceeb54a53e29f876ce0f61b933f92f
tcodling <thomas.codling@outlook.com>
2020-04-15 12:07:20 +1200

    add link for each fighter

--- client/components/Rankings.jsx
+++ client/components/Rankings.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import {Link} from 'react-router-dom'
+
 class Rankings extends React.Component {
     state = {
         expand: false
@@ -15,7 +17,7 @@ class Rankings extends React.Component {
             <h1 onClick={this.handleClick}>{this.props.name}</h1>
             {this.state.expand ? 
                 this.props.competitors.map(competitor => 
-                    <h2 className='name'>{competitor.rank}: {competitor.competitor.name}</h2>) : <h2></h2>}
+                    <h2 className='name'><Link to={`fighter/${competitor.competitor.id}`}>{competitor.rank}: {competitor.competitor.name}</Link></h2>) : <h2></h2>}
             </div>
         )
     }
